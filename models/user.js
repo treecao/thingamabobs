1e257296504fd53d86247b9fd6e51b875c382a2c
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-03-25 20:37:03 -0500

    added user files for signup page to store data in to database

--- models/user.js
+++ models/user.js
@@ -42,10 +42,6 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
     },
     sequelize,
     timestamps: false,
