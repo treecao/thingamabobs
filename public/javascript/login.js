574ed46e27678b2a6003098eae5da635abed2249
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-03-27 21:50:57 -0500

    sign up and login functionality working sessions (#47)
    commited out for now.

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -5,7 +5,7 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
-      const response = await fetch('/api/users/login', {
+      const response = await fetch('/api/user/login', {
         method: 'post',
         body: JSON.stringify({
           email,
@@ -22,5 +22,5 @@ const loginFormHandler = async (event) => {
       }
     }
   }
-
+ 
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
