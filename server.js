a5eeeb33785a81a23cabc781a5fb9ca338843462
dbradley555 <dbradley5555@gmail.com>
2023-03-28 23:20:01 -0500

    resolve conflicts in server.js

--- server.js
+++ server.js
@@ -39,13 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const hbs = exphbs.create({});
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
-app.use(require("./controllers"));
-
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
