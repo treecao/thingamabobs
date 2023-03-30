9d3c125e36163d2376c49291bdaf387592c5d5d5
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-03-30 17:57:59 -0500

    added css on add to cart button and local host to link inside terminal

--- server.js
+++ server.js
@@ -42,5 +42,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`app listening at http://localhost:${PORT}`));
 });
