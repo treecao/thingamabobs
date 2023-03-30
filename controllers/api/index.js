b2e47b9ab0a924fdbfac885ca9d9ecbe0e83fab0
dbradley555 <dbradley5555@gmail.com>
2023-03-30 18:48:24 -0500

    fix connection issue

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const catalogRoutes = require('./catalog-routes');
 
 router.use('/user', userRoutes);
-router.use('/catalog', catalogRoutes);
 
 module.exports = router;
