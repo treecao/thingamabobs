7fc4bc382b27ecff020df4150ad66fdebb10d7be
dbradley555 <35978190+dbradley555@users.noreply.github.com>
2023-03-29 20:33:48 -0500

    Revert "Mnguyen/newsession (#59)"
    This reverts commit 487be52b689d211f8c3b73ad964e86ff478a733b.

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,86 +1,94 @@
-const router = require("express").Router();
-const { Product, Category, Tag, TagCategory } = require("../models");
-const withAuth = require("../utils/auth");
+const router = require('express').Router();
+const { Product, Category, Tag, TagCategory } = require('../models');
 
 // route to get to landing HOME page
 
-router.get("/", async (req, res) => {
-  res.render("homepage", {
-    username: req.session.username,
-    loggedIn: req.session.logged_in,
-  });
+router.get('/', async (req, res) => {
+  res.render('homepage', {username:req.session.username,loggedIn:req.session.logged_in}); 
+
 });
 
 //route to login page
-router.get("/login", async (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
-  res.render("login");
+router.get('/login', async (req, res) => {
+  res.render('login');
 });
 
 // route to user sign up
-router.get("/sign-up", async (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
-  res.render("signup");
+router.get('/sign-up', async (req, res) => {
+  res.render('signup');
 });
 
 //route to shopping cart
-router.get("/cart", withAuth, async (req, res) => {
-  res.render("cart");
+router.get('/cart', async (req, res) => {
+  res.render('cart');
 });
 
-//route to profile
-router.get("/profile", withAuth, async (req, res) => {});
-
 //route to catalog
-router.get("/catalog", async (req, res) => {
+router.get('/catalog', async (req, res) => {
   try {
     const productData = await Product.findAll();
+    const categoryData = await Category.findAll();
 
     const products = productData.map((product) => product.get({ plain: true }));
-    console.log(products);
-    res.render("catalog", {
-      products,
-      username: req.session.username,
-      loggedIn: req.session.logged_in,
-    });
+    const categories = categoryData.map((category) =>
+      category.get({ plain: true })
+    );
+    res.render('catalog', { products, categories });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 //route to specific category in catalog
-router.get("/catalog/category/:id", async (req, res) => {
+router.get('/category/:id', async (req, res) => {
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [
-        { model: Product, attributes: ["product_name", "img", "price"] },
-        { model: TagCategory, attributes: ["tag_category_name"] },
-        { model: Tag, attributes: ["tag_name"] },
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'img', 'price'],
+        },
       ],
     });
-    const categories = categoryData.map((category) =>
-      category.get({ plain: true })
-    );
-    console.log(categories);
-    res.render("category", { categories });
+
+    const category = categoryData.get({ plain: true });
+    console.log(category);
+    res.render('category', {
+      ...category,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// route to specific product
+router.get('/product/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Tag,
+          attributes: ['tag_name'],
+        },
+      ],
+    });
+
+    const product = productData.get({ plain: true });
+    console.log(product);
+    res.render('product', {
+      ...product,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 //route to Contact page
-router.get("/contact", async (req, res) => {
-  res.render("contact");
+router.get('/contact', async (req, res) => {
+  res.render('contact');
 });
 
 //route to create Post to add to catalog
-router.get("/sell", async (req, res) => {
-  res.render("sell");
+router.get('/sell', async (req, res) => {
+  res.render('sell');
 });
 
 module.exports = router;
