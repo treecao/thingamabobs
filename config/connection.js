365d071f9cdc343d97796a8790e76c2be254f747
treecoww <tcao89@gmail.com>
2023-03-30 21:46:02 -0500

    connected to heroku & fixed product logo

--- config/connection.js
+++ config/connection.js
@@ -21,4 +21,16 @@ if (process.env.JAWSDB_URL) {
   );
 }
 
+// if (process.env.DATABASE_URL) {
+//   // the application is executed on Heroku ... use the postgres database
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     dialect:  'postgres',
+//     protocol: 'postgres',
+//     logging:  true //false
+//   });
+// } else {
+//   // the application is executed on the local machine
+//   sequelize = new Sequelize("postgres:///my_db");
+// }
+
 module.exports = sequelize;
